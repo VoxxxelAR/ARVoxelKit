ec758a75d62d1a97fe7ffee864715e459bee0c99
Gleb Radchenko <rgs444@mail.ru>
2017-09-26 19:54:59 +0300

    Project initial setup

---
+++ ARBoxKit/ARBoxKit/ARBoxKit.h
@@ -0,0 +1,19 @@
+//
+//  ARBoxKit.h
+//  ARBoxKit
+//
+//  Created by Gleb Radchenko on 9/26/17.
+//  Copyright © 2017 Gleb Radchenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ARBoxKit.
+FOUNDATION_EXPORT double ARBoxKitVersionNumber;
+
+//! Project version string for ARBoxKit.
+FOUNDATION_EXPORT const unsigned char ARBoxKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ARBoxKit/PublicHeader.h>
+
+
