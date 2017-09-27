87d581e620dbf16699c26b5f64548f891841310e
Gleb Radchenko <rgs444@mail.ru>
2017-09-27 16:43:33 +0300

    Move AR Session and Display logic to BKSceneManager

---
+++ ARBoxKit/ARBoxKit/Sources/Managers/BKSceneManager.swift
@@ -0,0 +1,327 @@
+//
+//  BKSceneManager.swift
+//  ARBoxKit
+//
+//  Created by Gleb on 9/27/17.
+//  Copyright © 2017 Gleb Radchenko. All rights reserved.
+//
+
+import Foundation
+import ARKit
+import SceneKit
+
+public protocol BKSceneManagerDelegate: class {
+    var voxelSize: CGFloat { get }
+    
+    func bkSceneManager(_ manager: BKSceneManager, shouldResetSessionFor state: BKARSessionState) -> Bool
+    func bkSceneManager(_ manager: BKSceneManager, stateUpdated newState: BKARSessionState)
+}
+
+extension BKSceneManagerDelegate {
+    public var voxelSize: CGFloat {
+        return BKConstants.voxelSideLength
+    }
+    
+    public func bkSceneManager(_ manager: BKSceneManager, shouldResetSessionFor state: BKARSessionState) -> Bool {
+        return true
+    }
+    
+    public func bkSceneManager(_ manager: BKSceneManager, stateUpdated newState: BKARSessionState) { }
+}
+
+open class BKSceneManager: NSObject {
+    public var voxelSize: CGFloat {
+        didSet {
+            guard abs(voxelSize - oldValue) > 0.01 else { return }
+            //TODO: - Update scene
+        }
+    }
+    
+    public var state: BKARSessionState = .limitedInitializing {
+        didSet {
+            delegate?.bkSceneManager(self, stateUpdated: state)
+        }
+    }
+    
+    public var platformState: BKPlatformState = .empty
+    public var platforms: [ARPlaneAnchor: BKPlatformNode] = [:]
+    
+    var updateQueue: DispatchQueue = DispatchQueue(label: "ARBoxKit-scene-update-queue")
+    
+    public weak var scene: ARSCNView!
+    var session: ARSession {
+        return scene.session
+    }
+    
+    weak var delegate: BKSceneManagerDelegate?
+    
+    init(with scene: ARSCNView) {
+        self.scene = scene
+        self.voxelSize = BKConstants.voxelSideLength
+        
+        super.init()
+        
+        setup()
+    }
+    
+    //MARK: - Setup
+    func setup() {
+        UIApplication.shared.isIdleTimerDisabled = true
+        
+        setupScene()
+        setupCamera()
+    }
+    
+    func setupScene() {
+        scene.delegate = self
+        session.delegate = self
+        
+        scene.scene = SCNScene()
+        scene.automaticallyUpdatesLighting = true
+        
+        if BKConstants.debug {
+            scene.showsStatistics = true
+            scene.debugOptions = [ARSCNDebugOptions.showWorldOrigin,
+                                  ARSCNDebugOptions.showFeaturePoints]
+        }
+    }
+    
+    func setupCamera() {
+        guard let camera = scene.pointOfView?.camera else { return }
+        
+        camera.wantsHDR = true
+        camera.exposureOffset = -1
+        camera.minimumExposure = -1
+        camera.maximumExposure = 3
+    }
+    
+    //MARK: - Session Managing
+    public func launchSession() {
+        let configuration = state.configuration
+        session.run(configuration)
+    }
+    
+    public func pauseSession() {
+        session.pause()
+    }
+    
+    public func resetSession() {
+        let configuration = state.configuration
+        session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
+    }
+}
+
+//MARK: - Logic
+extension BKSceneManager {
+    func updateFocus() {
+        switch state {
+        case .normal(let platformState):
+            switch platformState {
+            case .empty, .focused:
+                updatePlatformsFocus()
+            case .selected:
+                updateTilesFocus()
+                updateBoxesFocus()
+            }
+        default:
+            break
+        }
+    }
+}
+
+//MARK: - Box processing
+extension BKSceneManager {
+    func updateBoxesFocus() {
+        
+    }
+}
+
+//MARK: - Tile processing
+extension BKSceneManager {
+    func updateTilesFocus() {
+        
+    }
+}
+
+//MARK: - Platform processing
+extension BKSceneManager {
+    func updatePlatformsFocus() {
+        guard let result = scene.hitTestNode(from: scene.center, nodeType: BKPlatformNode.self) else {
+            unhilghlightAllPlatforms()
+            return
+        }
+        
+        guard let platform = result.node as? BKPlatformNode else { return }
+        
+        guard let face = BKBoxFace(rawValue: result.geometryIndex) else {
+            debugPrint("Wrong face index")
+            return
+        }
+        
+        switch platformState {
+        case .focused(let currentFocusedPlatform):
+            currentFocusedPlatform.updateState(newState: .normal, true, nil)
+        default:
+            unhilghlightAllPlatforms(except: platform)
+        }
+        
+        platform.updateState(newState: .highlighted(face: [face], alpha: 0.2), true, nil)
+        platformState = .focused(platform: platform)
+    }
+    
+    func unhilghlightAllPlatforms(except node: BKPlatformNode? = nil) {
+        platforms.values.forEach { (platform) in
+            if platform == node { return }
+            platform.updateState(newState: .normal, true, nil)
+        }
+    }
+}
+
+//MARK: - ARSCNViewDelegate
+extension BKSceneManager: ARSCNViewDelegate {
+    public func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
+        DispatchQueue.main.async {
+            self.updateFocus()
+        }
+    }
+    
+    public func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
+        
+        let platform = BKPlatformNode(anchor: planeAnchor)
+        
+        platforms[planeAnchor] = platform
+        node.addChildNode(platform)
+    }
+    
+    public func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor, let platform = platforms[planeAnchor] else { return }
+        updateQueue.async {
+            platform.update(planeAnchor, animated: true)
+        }
+    }
+    
+    public func renderer(_ renderer: SCNSceneRenderer, didRemove node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
+        
+        platforms[planeAnchor] = nil
+    }
+}
+
+public enum BKARSessionState {
+    case normal(BKPlatformState)
+    case normalEmptyAnchors
+    case notAvailable
+    case limitedExcessiveMotion
+    case limitedInsufficientFeatures
+    case limitedInitializing
+    
+    case interrupted
+    case interruptionEnded
+    case failed(Error)
+    
+    var configuration: ARWorldTrackingConfiguration {
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.worldAlignment = .gravityAndHeading
+        
+        switch self {
+        case .normal(let platformState):
+            switch platformState {
+            case .selected:
+                break
+            default:
+                configuration.planeDetection = .horizontal
+            }
+        default:
+            configuration.planeDetection = .horizontal
+        }
+        
+        return configuration
+    }
+    
+    public var hint: String {
+        switch self {
+        case .normal(let platformState):
+            switch platformState {
+            case .selected:
+                return ""
+            default:
+                return "Select platform"
+            }
+        case .normalEmptyAnchors:
+            return "Move the device around to detect horizontal surfaces."
+        case .notAvailable:
+            return "Tracking unavailable."
+        case .limitedExcessiveMotion:
+            return "Tracking limited - Move the device more slowly."
+        case .limitedInsufficientFeatures:
+            return "Tracking limited - Point the device at an area with visible surface detail, or improve lighting conditions."
+        case .limitedInitializing:
+            return "Initializing AR session."
+        case .interrupted:
+            return "Session was interrupted"
+        case .interruptionEnded:
+            return "Session interruption ended"
+        case .failed(let error):
+            return "Session failed: \(error.localizedDescription)"
+        }
+    }
+}
+
+//MARK: - ARSessionDelegate
+extension BKSceneManager: ARSessionDelegate {
+    func updateState(for frame: ARFrame, trackingState: ARCamera.TrackingState) {
+        switch trackingState {
+        case .normal where frame.anchors.isEmpty:
+            state = .normalEmptyAnchors
+        case .normal:
+            state = .normal(platformState)
+        case .notAvailable:
+            state = .notAvailable
+        case .limited(.excessiveMotion):
+            state = .limitedExcessiveMotion
+        case .limited(.insufficientFeatures):
+            state = .limitedInsufficientFeatures
+        case .limited(.initializing):
+            state = .limitedInitializing
+        }
+    }
+    
+    public func session(_ session: ARSession, didAdd anchors: [ARAnchor]) {
+        guard let frame = session.currentFrame else { return }
+        updateState(for: frame, trackingState: frame.camera.trackingState)
+    }
+    
+    public func session(_ session: ARSession, didRemove anchors: [ARAnchor]) {
+        guard let frame = session.currentFrame else { return }
+        updateState(for: frame, trackingState: frame.camera.trackingState)
+    }
+    
+    public func session(_ session: ARSession, cameraDidChangeTrackingState camera: ARCamera) {
+        guard let frame = session.currentFrame else { return }
+        updateState(for: frame, trackingState: camera.trackingState)
+    }
+    
+    // MARK: - ARSessionObserver
+    public func sessionWasInterrupted(_ session: ARSession) {
+        state = .interrupted
+    }
+    
+    public func sessionInterruptionEnded(_ session: ARSession) {
+        state = .interruptionEnded
+        
+        let shouldReset = delegate?.bkSceneManager(self, shouldResetSessionFor: state) ?? true
+        if shouldReset {
+            resetSession()
+        }
+    }
+    
+    public func session(_ session: ARSession, didFailWithError error: Error) {
+        state = .failed(error)
+        
+        let shouldReset = delegate?.bkSceneManager(self, shouldResetSessionFor: state) ?? true
+        if shouldReset {
+            resetSession()
+        }
+    }
+}
--- ARBoxKit/ARBoxKit/Sources/ViewControllers/BKViewController.swift
+++ ARBoxKit/ARBoxKit/Sources/ViewControllers/BKViewController.swift
@@ -10,34 +10,6 @@ import UIKit
 import ARKit
 import SceneKit
 
-public enum BKViewControllerState {
-    case sessionInitializing
-    case sessionPrepared
-    case sessionPaused
-    case sessionError(message: String)
-    
-    case platformSelection
-    
-    case working
-    
-    var sessionConfiguration: ARWorldTrackingConfiguration {
-        switch self {
-        case .working:
-            let configuration = ARWorldTrackingConfiguration()
-            configuration.planeDetection = .horizontal
-            configuration.worldAlignment = .gravityAndHeading
-            
-            return configuration
-        default:
-            let configuration = ARWorldTrackingConfiguration()
-            configuration.planeDetection = .horizontal
-            configuration.worldAlignment = .gravityAndHeading
-            
-            return configuration
-        }
-    }
-}
-
 public enum BKPlatformState {
     case empty
     case focused(platform: BKPlatformNode)
@@ -45,195 +17,45 @@ public enum BKPlatformState {
 }
 
 open class BKViewController: UIViewController {
-    
-    public var platforms: [ARPlaneAnchor: BKPlatformNode] = [:]
-    public var state: BKViewControllerState = .sessionInitializing
-    public var platformState: BKPlatformState = .empty
-    
     @IBOutlet open var sceneView: ARSCNView!
     
     weak var statusView: UIView?
     weak var statusLabel: UILabel?
     
-    var session: ARSession {
-        return sceneView.session
-    }
-    
-    var updateQueue: DispatchQueue = DispatchQueue(label: "ARBoxKit-scene-update-queue")
+    var sceneManager: BKSceneManager?
     
     override open func viewDidLoad() {
         super.viewDidLoad()
         
-        UIApplication.shared.isIdleTimerDisabled = true
-        
-        setupScene()
-        setupCamera()
-        
-        if BKConstants.debug {
-            addStatusLabel()
-        }
+        sceneManager = BKSceneManager(with: sceneView)
+        sceneManager?.delegate = self
+        setupUI()
     }
     
     override open func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        
-        launchSession()
+        sceneManager?.launchSession()
     }
     
     override open func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        
-        state = .sessionPaused
-        session.pause()
+        sceneManager?.pauseSession()
     }
     
     func setupUI() {
-        
-    }
-    
-    func setupScene() {
-        sceneView.delegate = self
-        session.delegate = self
-        
-        sceneView.scene = SCNScene()
-        
-        sceneView.showsStatistics = true
-        sceneView.automaticallyUpdatesLighting = true
-        sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin,
-                                  ARSCNDebugOptions.showFeaturePoints]
-    }
-    
-    func setupCamera() {
-        guard let camera = sceneView.pointOfView?.camera else {
-            fatalError("Expected a valid `pointOfView` from the scene.")
-        }
-        
-        camera.wantsHDR = true
-        camera.exposureOffset = -1
-        camera.minimumExposure = -1
-        camera.maximumExposure = 3
-    }
-    
-    func launchSession() {
-        let configuration = state.sessionConfiguration
-        state = .sessionInitializing
-        
-        session.run(configuration)
-        
-        switch platformState {
-        case .empty, .focused:
-            state = .platformSelection
-        case .selected:
-            state = .working
-        }
-    }
-    
-    func resetSession() {
-        let configuration = state.sessionConfiguration
-        state = .sessionInitializing
-        
-        session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
-        
-        switch platformState {
-        case .empty, .focused:
-            state = .platformSelection
-        case .selected:
-            state = .working
-        }
-    }
-    
-    func updateState(for frame: ARFrame, trackingState: ARCamera.TrackingState) {
-        var message: String = ""
-        
-        switch platformState {
-        case .empty, .focused:
-            state = .platformSelection
-            message = "Platform selection"
-        case .selected:
-            state = .working
-            message = ""
-        }
-        
-        switch trackingState {
-        case .normal where frame.anchors.isEmpty:
-            message = "Move the device around to detect horizontal surfaces."
-        case .normal:
-            break
-        case .notAvailable:
-            message = "Tracking unavailable."
-            state = .sessionError(message: message)
-        case .limited(.excessiveMotion):
-            message = "Tracking limited - Move the device more slowly."
-        case .limited(.insufficientFeatures):
-            message = "Tracking limited - Point the device at an area with visible surface detail, or improve lighting conditions."
-        case .limited(.initializing):
-            message = "Initializing AR session."
-            state = .sessionInitializing
-        }
-        
-        statusLabel?.text = message
-        statusView?.isHidden = message.isEmpty
-    }
-}
-
-//MARK: - Logic
-extension BKViewController {
-    public func updateFocus() {
-        switch state {
-        case .platformSelection:
-            updatePlatformsFocus()
-        case .working:
-            return
-        default:
-            break
+        if BKConstants.debug {
+            addStatusLabel()
         }
     }
 }
 
-//MARK: - Box processing
-extension BKViewController {
-    
-}
-
-//MARK: - Tile processing
-extension BKViewController {
-    
-}
-
-//MARK: - Platform processing
-extension BKViewController {
-    func updatePlatformsFocus() {
-        guard let result = sceneView.hitTestNode(from: sceneView.center, nodeType: BKPlatformNode.self) else {
-            unhilghlightAllPlatforms()
-            return
-        }
-        
-        guard let platform = result.node as? BKPlatformNode else { return }
-        
-        guard let face = BKBoxFace(rawValue: result.geometryIndex) else {
-            debugPrint("Wrong face index")
-            return
-        }
-        
-        switch platformState {
-        case .focused(let currentFocusedPlatform):
-            currentFocusedPlatform.updateState(newState: .normal, true, nil)
-        default:
-            unhilghlightAllPlatforms(except: platform)
-        }
-        
-        platform.updateState(newState: .highlighted(face: [face], alpha: 0.2), true, nil)
-        platformState = .focused(platform: platform)
-    }
-    
-    func unhilghlightAllPlatforms(except node: BKPlatformNode? = nil) {
-        platforms.values.forEach { (platform) in
-            if platform == node { return }
-            platform.updateState(newState: .normal, true, nil)
-        }
+extension BKViewController: BKSceneManagerDelegate {
+    public func bkSceneManager(_ manager: BKSceneManager, stateUpdated newState: BKARSessionState) {
+        print(newState)
+        statusLabel?.text = newState.hint
+        statusView?.isHidden = newState.hint.isEmpty
     }
 }
-
 //MARK: - UI Setuping
 extension BKViewController {
     func addStatusLabel() {
@@ -270,68 +92,5 @@ extension BKViewController {
     }
 }
 
-extension BKViewController: ARSCNViewDelegate {
-    public func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
-        DispatchQueue.main.async {
-            self.updateFocus()
-        }
-    }
-    
-    public func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
-        guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
-        
-        let platform = BKPlatformNode(anchor: planeAnchor)
-        
-        platforms[planeAnchor] = platform
-        node.addChildNode(platform)
-    }
-    
-    public func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
-        guard let planeAnchor = anchor as? ARPlaneAnchor, let platform = platforms[planeAnchor] else { return }
-        updateQueue.async {
-            platform.update(planeAnchor, animated: true)
-        }
-    }
-    
-    public func renderer(_ renderer: SCNSceneRenderer, didRemove node: SCNNode, for anchor: ARAnchor) {
-        guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
-        
-        platforms[planeAnchor] = nil
-    }
-}
 
-extension BKViewController: ARSessionDelegate {
-    public func session(_ session: ARSession, didAdd anchors: [ARAnchor]) {
-        guard let frame = session.currentFrame else { return }
-        updateState(for: frame, trackingState: frame.camera.trackingState)
-    }
-    
-    public func session(_ session: ARSession, didRemove anchors: [ARAnchor]) {
-        guard let frame = session.currentFrame else { return }
-        updateState(for: frame, trackingState: frame.camera.trackingState)
-    }
-    
-    public func session(_ session: ARSession, cameraDidChangeTrackingState camera: ARCamera) {
-        guard let frame = session.currentFrame else { return }
-        updateState(for: frame, trackingState: camera.trackingState)
-    }
-    
-    // MARK: - ARSessionObserver
-    
-    public func sessionWasInterrupted(_ session: ARSession) {
-        statusLabel?.text = "Session was interrupted"
-    }
-    
-    public func sessionInterruptionEnded(_ session: ARSession) {
-        statusLabel?.text = "Session interruption ended"
-        resetSession()
-    }
-    
-    public func session(_ session: ARSession, didFailWithError error: Error) {
-        let message = "Session failed: \(error.localizedDescription)"
-        statusLabel?.text = message
-        state = .sessionError(message: message)
-        resetSession()
-    }
-}
 
