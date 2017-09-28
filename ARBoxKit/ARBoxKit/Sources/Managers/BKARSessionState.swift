d3049c47a62158cc2d24e533f96a28ced284efe0
Gleb Radchenko <rgs444@mail.ru>
2017-09-28 09:02:09 +0300

    replace SceneManager logic to different files

---
+++ ARBoxKit/ARBoxKit/Sources/Managers/BKARSessionState.swift
@@ -0,0 +1,9 @@
+//
+//  BKSessionState.swift
+//  ARBoxKit
+//
+//  Created by Gleb Radchenko on 9/28/17.
+//  Copyright © 2017 Gleb Radchenko. All rights reserved.
+//
+
+import Foundation
---
+++ ARBoxKit/ARBoxKit/Sources/Managers/BKSceneFocusContainer.swift
@@ -0,0 +1,9 @@
+//
+//  BKSceneFocusContainer.swift
+//  ARBoxKit
+//
+//  Created by Gleb Radchenko on 9/28/17.
+//  Copyright © 2017 Gleb Radchenko. All rights reserved.
+//
+
+import Foundation
