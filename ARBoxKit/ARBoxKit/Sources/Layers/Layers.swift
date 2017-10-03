b4b0e8b2513613e6d697b9c75a9b5cdb372f9035
Gleb Radchenko <rgs444@mail.ru>
2017-10-03 12:10:31 +0300

    add test appearance for boxes

---
+++ ARBoxKit/ARBoxKit/Sources/Layers/Layers.swift
@@ -0,0 +1,87 @@
+//
+//  Layers.swift
+//  ARBoxKit
+//
+//  Created by Gleb on 10/3/17.
+//  Copyright © 2017 Gleb Radchenko. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+
+class BorderedLayer: CALayer {
+    override var frame: CGRect {
+        didSet {
+            borderWidth = frame.width / 20
+        }
+    }
+    
+    override init() {
+        super.init()
+        
+        borderColor = UIColor.black.cgColor
+        borderWidth = 3
+        shouldRasterize = true
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
+
+class ColoredLayer: BorderedLayer {
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    init(color: UIColor) {
+        super.init()
+        backgroundColor = color.cgColor
+    }
+}
+
+class TexturedLayer: BorderedLayer {
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    init(image: UIImage) {
+        super.init()
+        
+        contents = image.cgImage
+        contentsGravity = kCAGravityResizeAspectFill
+        masksToBounds = true
+        shouldRasterize = true
+    }
+}
+
+class GradientedLayer: CAGradientLayer {
+    
+    override var frame: CGRect {
+        didSet {
+            borderWidth = frame.width / 20
+        }
+    }
+    
+    init(colors: [UIColor]) {
+        super.init()
+        
+        borderColor = UIColor.black.cgColor
+        borderWidth = 3
+        
+        self.colors = colors.flatMap { $0.cgColor }
+        shouldRasterize = true
+    }
+    
+    convenience init(colors: [UIColor], start: CGPoint = .zero, end: CGPoint) {
+        self.init(colors: colors)
+        
+        startPoint = start
+        endPoint = end
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
--- ARBoxKit/ARBoxKit/Sources/Nodes/BKBoxNode.swift
+++ ARBoxKit/ARBoxKit/Sources/Nodes/BKBoxNode.swift
@@ -19,29 +19,34 @@ open class BKBoxNode: SCNNode, BoxDisplayable {
     
     init(sideLength: CGFloat) {
         super.init()
-        
         geometry = SCNBox(sideLength: sideLength)
-        
         setupGeometry()
         
-        let image = UIImage(named: "square_frame", in: Bundle.init(for: type(of: self)), compatibleWith: nil)
-        BKBoxFace.all.forEach { (face) in
+        let blue = UIColor(red: 51 / 255, green: 171 / 255, blue: 224 / 255, alpha: 1.0)
+        let image = UIImage(named: "pattern_1", in: Bundle(for: type(of: self)), compatibleWith: nil)!
+        
+        let differentLayers: [CALayer] = [TexturedLayer(image: image),
+                                          ColoredLayer(color: blue),
+                                          GradientedLayer(colors: [.white, blue]),
+                                          ColoredLayer(color: blue),
+                                          GradientedLayer(colors: [.white, blue]),
+                                          TexturedLayer(image: image)]
+        
+        differentLayers.forEach { $0.frame = CGRect(x: 0,
+                                                    y: 0,
+                                                    width: 200,
+                                                    height: 200) }
+        
+        BKBoxFace.all.enumerated().forEach { (index, face) in
             let material = boxMaterial(for: face)
-            material.diffuse.contents = image
+            material.diffuse.contents = differentLayers[index]
         }
-//        geometry?.firstMaterial?.diffuse.contents = image!
-//        geometry?.firstMaterial?.isDoubleSided = true
-//        geometry?.firstMaterial?.locksAmbientWithDiffuse = true
-        //applyColors()
     }
     
     required public init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-        
         geometry = SCNBox()
-        
         setupGeometry()
-        applyColors()
     }
 }
 
